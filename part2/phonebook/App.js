2ff58a308355fec282abb9ca3780c9da9f2299c4
Facu <bestjayceeu@gmail.com>
2022-11-25 18:32:12 +0100

    2.11: The Phonebook Step6

--- part2/phonebook/App.js
+++ part2/phonebook/App.js
@@ -1,25 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter.js'
 import PersonForm from './components/PersonForm.js'
 import Persons from './components/Persons.js'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
-
-  /*
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 0, number: 444444444 }
-  ])
-  */
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [findPerson, setFindPerson] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const filterArray = findPerson.length === 0 ? persons 
   : persons.filter(e=>e.name.includes(findPerson))
